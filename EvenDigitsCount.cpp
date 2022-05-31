864bca47d973de298cd3399b5fd7ccabcfe801c3
AW1ng <87144424+AW1ng@users.noreply.github.com>
2022-05-31 20:26:31 +0300

    Update EvenDigitsCount.cpp

--- EvenDigitsCount.cpp
+++ EvenDigitsCount.cpp
@@ -12,11 +12,34 @@ void EvenCount(long long N, int &ans) {
 	return;
 }
 
+void _EvenCount(long long N, int &ans) {
+	int modN;
+	for(int i = 0; N > 0; i++) {
+		modN = N % 10;
+		N /= 10;
+		if(modN % 2 == 0) ans++;
+	}
+	
+	return;
+}
+
+void RecEvenCount(long long N, int &ans) {
+	int modN = N % 10;
+	if(N == 0) return;
+	if(modN % 2 == 0) ans++;
+	return RecEvenCount( N / 10, ans);
+}
 
 int main() {
 	int ans;
 	
-	EvenCount(9223372036854775806,ans);
-	cout <<"Answer is " << ans;
-}
+//	EvenCount(9223372036854775806,ans);
+//	cout <<"Answer is " << ans;
 
+//	_EvenCount(9223372036854775806,ans);
+//	cout <<"Answer is " << ans;
+
+	RecEvenCount(9223372036854775806,ans);
+	cout <<"Answer is " << ans;
+	
+}
